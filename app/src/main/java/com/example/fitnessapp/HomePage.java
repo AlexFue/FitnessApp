dde16ef6390152f1bc51718e87dbd6a9187b66cb
Alex <alexespinoza11f@gmail.com>
2021-08-31 11:02:13 -0700

    Created Homepage for login/create account buttons that go to those pages. No tests need for this issue.

---
+++ app/src/main/java/com/example/fitnessapp/HomePage.java
@@ -0,0 +1,39 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    Button btn_login;
+    Button btn_create;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_create = findViewById(R.id.btn_create);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this, CreateAccount.class);
+                startActivity(i);
+            }
+        });
+
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this, Login.class);
+                startActivity(i);
+            }
+        });
+    }
+}
