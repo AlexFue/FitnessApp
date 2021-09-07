8f99fff36794236f8bcaa2d52eb2af548988bcb8
JeremyMeh <jeremymm125@gmail.com>
2021-09-07 11:21:34 -0700

    This commit will make the login page work with the database by checking the real variables for the user with the username and password typed.

--- app/src/main/java/com/example/fitnessapp/LoginPage.java
+++ app/src/main/java/com/example/fitnessapp/LoginPage.java
@@ -9,12 +9,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity {
-
+    private FitnessAppDB fdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        fdb = FitnessAppDB.getInstance(this);
     }
 
     public void signIn(View view){
@@ -24,15 +25,19 @@ public class LoginPage extends AppCompatActivity {
         String user = String.valueOf(username.getText());
         String pass = String.valueOf(password.getText());
 
+        User userExist = fdb.user().findUserByUsername(user);
+
+        String real_user = userExist.getUsername();
+        String real_pass = userExist.getPassword();
 
         if(user.equals("") || pass.equals("")){
             String error_msg = "Fill in both fields.";
             Toast.makeText(getApplicationContext(), error_msg, Toast.LENGTH_LONG).show();
         }
-//        else if(user.equals(name_creds) && pass.equals(pass_creds)){ // this needs to be checked with the credentials take from the database
-//            Intent i = new Intent(this, MainWorkoutPage.class); // this loads the main workout page with all the list of workouts
-//            startActivity(i);
-//        }
+        else if(real_user.equals(user) && real_pass.equals(pass)){
+            Intent i = new Intent(this, MainWorkoutPage.class);
+            startActivity(i);
+        }
         else{
             String error_msg = "Incorrect Username or Password.";
             Toast.makeText(getApplicationContext(), error_msg, Toast.LENGTH_LONG).show();
