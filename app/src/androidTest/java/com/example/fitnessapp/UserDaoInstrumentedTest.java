cd801e1a0975163ceda11678d9185ebc936be17b
Alex <alexespinoza11f@gmail.com>
2021-09-06 22:54:10 -0700

    Tested user creation and methods. Also testing user queries from database.

---
+++ app/src/androidTest/java/com/example/fitnessapp/UserDaoInstrumentedTest.java
@@ -0,0 +1,43 @@
+package com.example.fitnessapp;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class UserDaoInstrumentedTest {
+
+    FitnessApp db;
+
+    @Test
+    public void test() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
+
+    // adds user to db and checks its therer
+    @Test
+    public void UserDaoTest() {
+
+    }
+
+    @Test
+    public void UserCredentialsTest() {
+        User u1 = new User("user1", "password1");
+        assertEquals(u1.getUsername(), "user1");
+        assertEquals(u1.getPassword(), "password");
+    }
+
+    @Test
+    public void UserfindUsernameTest() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        db = FitnessApp.getInstance(appContext);
+        User found = db.user().findUsername("alex");
+        assert(found != null);
+    }
+}
