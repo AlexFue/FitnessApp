37ad78f241c79e8b87c7eb1d5837fb85116241fd
Noah <noahahmed316@gmail.com>
2021-09-10 15:05:32 -0700

    removed "serialized body" from api response, it was returning null. still need to figure out how to remove the html tags from the description but that could be done as the page is finalized.

--- app/src/main/java/com/example/fitnessapp/Results.java
+++ app/src/main/java/com/example/fitnessapp/Results.java
@@ -1,11 +1,8 @@
 package com.example.fitnessapp;
 
-import com.google.gson.annotations.SerializedName;
-
 public class Results {
     private int id;
     private String name;
-    @SerializedName("body")
     private String description;
 
     public int getId() {
