7558682054b169be7aca6ad8a73e47a56663b6e2
JeremyMeh <jeremymm125@gmail.com>
2021-09-14 01:30:35 -0700

    This is just a first commit for the account edit page.

--- app/src/main/java/com/example/fitnessapp/AccountInfo.java
+++ app/src/main/java/com/example/fitnessapp/AccountInfo.java
@@ -1,4 +1,36 @@
 package com.example.fitnessapp;
 
-public class AccountInfo {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountInfo extends AppCompatActivity {
+    private FitnessAppDB fdb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_account);
+        fdb = FitnessAppDB.getInstance(this);
+        String username = fdb.user().getUsername();
+        String password = fdb.user().password();
+
+        TextView welcome = findViewById(R.id.user_welcome);
+        TextView user_chng = findViewById(R.id.username_change);
+        TextView pass_chng = findViewById(R.id.password_change);
+
+        welcome.setText("Welcome " + username + "!");
+        user_chng.setText(username);
+        pass_chng.setText(password);
+    }
+
+    public void edit(View view){
+        Intent i  = new Intent(this, EditAccount.class);
+        startActivity(i);
+    }
+
+
 }
