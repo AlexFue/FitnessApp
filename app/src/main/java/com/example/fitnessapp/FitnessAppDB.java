a8b7b22aaca6014666ace9567683f83b5fd022a5
Alex <alexespinoza11f@gmail.com>
2021-09-15 10:25:37 -0700

    fixed issue of not being able to login because of unupdated db version.

--- app/src/main/java/com/example/fitnessapp/FitnessAppDB.java
+++ app/src/main/java/com/example/fitnessapp/FitnessAppDB.java
@@ -7,7 +7,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {User.class}, version = 2, exportSchema = false)
+@Database(entities = {User.class}, version = 6, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class FitnessAppDB extends RoomDatabase {
     private static FitnessAppDB sInstance;
