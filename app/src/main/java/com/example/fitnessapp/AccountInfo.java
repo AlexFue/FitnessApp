5a7b15c8aab22e890111a093c9f8f468115d1fa5
JeremyMeh <jeremymm125@gmail.com>
2021-09-16 17:12:01 -0700

    This is updating the files with the new dependencies that work on the M1 Mac and fixes up the look of the AccountInfo and EditAccount pages.

--- app/src/main/java/com/example/fitnessapp/AccountInfo.java
+++ app/src/main/java/com/example/fitnessapp/AccountInfo.java
@@ -3,6 +3,7 @@ package com.example.fitnessapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +15,10 @@ public class AccountInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_account);
 
-        Bundle bund = bun;
+        Bundle bund = this.getIntent().getExtras();
 
-        String username = bund.username;
-        String password = bund.password;
+        String username = bund.getString("username");
+        String password = bund.getString("password");
 
         TextView welcome = findViewById(R.id.user_welcome);
         TextView user_chng = findViewById(R.id.username_change);
@@ -26,6 +27,15 @@ public class AccountInfo extends AppCompatActivity {
         welcome.setText("Welcome " + username + "!");
         user_chng.setText(username);
         pass_chng.setText(password);
+
+        Button back = findViewById(R.id.return_button);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void edit(View view){
--- app/src/main/java/com/example/fitnessapp/EditAccount.java
+++ app/src/main/java/com/example/fitnessapp/EditAccount.java
@@ -2,6 +2,7 @@ package com.example.fitnessapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,11 +18,18 @@ public class EditAccount extends AppCompatActivity {
         EditText user_change = findViewById(R.id.edit_user);
         EditText pass_change = findViewById(R.id.edit_password);
         EditText retype_pass = findViewById(R.id.retype_password);
+        Button back = findViewById(R.id.return_button);
 
-
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void updateAccount(View v){
+
 
     }
 
